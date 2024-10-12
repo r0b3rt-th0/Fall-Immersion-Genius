ff3c2e50ee1620aebb5d5ef8b938ec0123fe21d7
r0b3rt-th0 <robert.thomas@hgs.hiddengeniusproject.org>
2024-10-12 10:45:51 -0400

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,4 @@
 function counter() {
-// Write your code below. Hint: you can finish it in about 3-5 lines.
+  counter_number=document.getElementById('counter-number');
+  counter-number.innerHTML=parseInt(counter_number)+1;
 }
