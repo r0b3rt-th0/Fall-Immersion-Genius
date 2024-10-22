33f53e0b51d8507162aba9fc25aecf2c73525911
r0b3rt-th0 <robert.thomas@hgs.hiddengeniusproject.org>
2024-10-22 17:52:49 -0400

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,12 @@
 function darkMode() {
-// Your code here.
+ const variable_darkmode = document.body;
+  function darkmode(){
+    if(variable_darkmode.style.backgroundColor === 'white'){
+      variable_darkmode.style.backgroundColor = 'black';
+        variable_darkmode.style.backgroundColor = 'white';
+    }
+      else{
+        variable_name.style.backgroundColo = 'white';
+        variable_name.style.color = 'black';
+      }
 }
