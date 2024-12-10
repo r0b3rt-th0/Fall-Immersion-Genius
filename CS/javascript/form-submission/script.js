6f864d8e9bc4fa245a4fb75f88218ac98d574b63
r0b3rt-th0 <robert.thomas@hgs.hiddengeniusproject.org>
2024-12-10 17:59:02 -0500

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,17 @@
 function formSubmission() {
-    // Your code here.
+    document.addEventListener ("DOMContentLoaded", () => {
+     const form = document.getElementById("myForm")
+     form.addEventListener ('submit', function (event) ){
+          event.preventDefault)
+          name = document.getElementById("name")
+          email = document.getElementById("email")
+          message = document.getElementById("message")
+          if (name && email && message) {
+               alert('Form submitted successfully!\nName: ${name)\nEmail: ${email)\nMessage: ${message)');
+          }
+          else{
+                alert('Fill all the fields!');
+          }
+     });
+});
 }
